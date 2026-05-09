42e81d528313a2e9b985f8c4c10add91f7935c27
ThyagoRafael <thyagorafael57@gmail.com>
2026-05-08 21:56:29 -0300

    feat: adicionar botão para página de carrinho no main header

--- frontend/src/components/header/MainHeader/index.tsx
+++ frontend/src/components/header/MainHeader/index.tsx
@@ -1,18 +1,26 @@
 import logo from "../../../assets/logo.png";
 import { Link } from "react-router-dom";
 import styles from "./MainHeader.module.css";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function MainHeader() {
 	return (
 		<header className={styles.container}>
 			<Link
 				to="/"
-				className={styles.link}
+				className={styles.homeLink}
 			>
 				<img
 					src={logo}
-					alt="Logo Davies"
+					alt="Logo da Davies Ecommerce"
 				/>
+			</Link>
+
+			<Link
+				to="/carrinho"
+				className={styles.cartLink}
+			>
+				<FaShoppingCart size={25} />
 			</Link>
 		</header>
 	);
