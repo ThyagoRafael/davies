68b7b042bd1f0fc85e71ab9a1866b8d969d74a8d
ThyagoRafael <thyagorafael57@gmail.com>
2026-05-08 21:51:08 -0300

    refactor: remover o parâmetro userId das rotas

--- backend/src/routes/cart.routes.ts
+++ backend/src/routes/cart.routes.ts
@@ -5,7 +5,7 @@ import { authenticationMiddleware } from "../middlewares/auth.middleware.js";
 const cartRoutes = Router();
 const cartController = new CartController();
 
-cartRoutes.get("/:userId", authenticationMiddleware, cartController.getProducts);
-cartRoutes.put("/:userId", authenticationMiddleware, cartController.finishCart);
+cartRoutes.get("/", authenticationMiddleware, cartController.getProducts);
+cartRoutes.put("/", authenticationMiddleware, cartController.finishCart);
 
 export { cartRoutes };
