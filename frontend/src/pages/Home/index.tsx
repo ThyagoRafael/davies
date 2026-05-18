2a2dd8ee9459d2a8645a6b1b03fa5dc1efad9d4c
ThyagoRafael <thyagorafael57@gmail.com>
2026-05-18 20:25:39 -0300

    feat: adicionar funcionalidade de adicionar ao carrinho

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,21 +1,56 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
 import type { Product } from "../../types/Product";
 import { formatPrice } from "../../utils/formatPrice";
 import { getAllProducts } from "../../services/api/products";
+import { FaCartPlus } from "react-icons/fa";
+import { BsCartCheck } from "react-icons/bs";
+import { addToCart } from "../../services/api/cartItem";
+import { AppError } from "../../errors/AppError";
 
 export default function Home() {
 	const [products, setProducts] = useState<Product[]>([]);
+	const [addedToCart, setAddedToCart] = useState<Set<number>>(new Set());
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		const fetchData = async () => {
 			const data = await getAllProducts();
+
 			setProducts(data);
 		};
 
 		fetchData();
 	}, []);
+
+	const handleAddToCart = async (productId: number) => {
+		try {
+			const data = await addToCart(productId);
+
+			setAddedToCart((prev) => {
+				const newSet = new Set(prev);
+				newSet.add(productId);
+
+				return newSet;
+			});
+
+			alert(data.message);
+		} catch (error) {
+			if (error instanceof AppError) {
+				if (error.statusCode === 401) {
+					localStorage.removeItem("user");
+					navigate("/entrar");
+					return;
+				}
+
+				alert(error.message);
+				return;
+			}
+
+			alert("Erro inesperado");
+		}
+	};
 
 	return (
 		<section className={styles.container}>
@@ -40,14 +75,26 @@ export default function Home() {
 									<h3 className={styles.productName}>{product.name}</h3>
 								</Link>
 
-								<p className={styles.productPrice}>
-									<strong>{formatPrice(product.price)}</strong>
-								</p>
+								<div className={styles.cardFooter}>
+									<p className={styles.productPrice}>
+										<strong>{formatPrice(product.price)}</strong>
+									</p>
+
+									<button
+										onClick={() => handleAddToCart(product.id)}
+										disabled={addedToCart.has(product.id)}
+									>
+										{addedToCart.has(product.id) ? <BsCartCheck size={20} /> : <FaCartPlus size={20} />}
+									</button>
+								</div>
 							</article>
 						</li>
 					))
 				) : (
-					<p>Não há produtos disponíveis</p>
+					<>
+						<p>Erro ao carregar produtos</p>
+						<p>Recarregue a página</p>
+					</>
 				)}
 			</ul>
 		</section>
