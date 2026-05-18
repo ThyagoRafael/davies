6e56e36a1193dbfbdf072076388da4001e3b31b7
ThyagoRafael <thyagorafael57@gmail.com>
2026-05-18 20:32:22 -0300

    feat: adicionar funções para adicionar produto e para deletar todos os itens no carrinho

--- frontend/src/services/api/cartItem.ts
+++ frontend/src/services/api/cartItem.ts
@@ -5,6 +5,30 @@ import { AppError } from "../../errors/AppError";
 import type { UpdateAction } from "../../types/cartItem/updateAction";
 import { getUserStorage } from "../../helpers/getUserStorage";
 
+export async function addToCart(productId: number) {
+	try {
+		const user = getUserStorage();
+
+		const response = await api.post(`/cart/items/${productId}`, null, {
+			headers: {
+				Authorization: `Bearer ${user.token}`,
+			},
+		});
+
+		return response.data;
+	} catch (error) {
+		if (error instanceof UnauthorizedError) {
+			throw new AppError(error.message, 401);
+		}
+
+		if (axios.isAxiosError(error)) {
+			throw new AppError(error.response?.data.message || "Erro ao buscar carrinho", error.response?.status || 500);
+		}
+
+		throw error;
+	}
+}
+
 export async function updateQuantityItem(action: UpdateAction, productId: number) {
 	try {
 		const user = getUserStorage();
@@ -22,12 +46,12 @@ export async function updateQuantityItem(action: UpdateAction, productId: number
 
 		return response.data;
 	} catch (error) {
-		if (axios.isAxiosError(error)) {
-			throw new AppError(error.response?.data.message || "Erro ao buscar carrinho", error.response?.status || 500);
-		}
-
 		if (error instanceof UnauthorizedError) {
 			throw new AppError(error.message, 401);
+		}
+
+		if (axios.isAxiosError(error)) {
+			throw new AppError(error.response?.data.message || "Erro ao buscar carrinho", error.response?.status || 500);
 		}
 
 		throw error;
@@ -39,6 +63,30 @@ export async function deleteItem(productId: number) {
 		const user = getUserStorage();
 
 		const response = await api.delete(`/cart/items/${productId}`, {
+			headers: {
+				Authorization: `Bearer ${user.token}`,
+			},
+		});
+
+		return response.data;
+	} catch (error) {
+		if (error instanceof UnauthorizedError) {
+			throw new AppError(error.message, 401);
+		}
+
+		if (axios.isAxiosError(error)) {
+			throw new AppError(error.response?.data.message || "Erro ao buscar carrinho", error.response?.status || 500);
+		}
+
+		throw error;
+	}
+}
+
+export async function deleteAllItems() {
+	try {
+		const user = getUserStorage();
+
+		const response = await api.delete("/cart/items", {
 			headers: {
 				Authorization: `Bearer ${user.token}`,
 			},
