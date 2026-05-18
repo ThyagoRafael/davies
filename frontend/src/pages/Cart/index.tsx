d553ca3c34db3a2de5b513170a08e1861525a0d2
ThyagoRafael <thyagorafael57@gmail.com>
2026-05-18 20:34:38 -0300

    feat: integrar funções de adicionar produto e deletar todos os produtos do carrinho e adicionar loading para disable de botões

--- frontend/src/pages/Cart/index.tsx
+++ frontend/src/pages/Cart/index.tsx
@@ -4,10 +4,10 @@ import { formatPrice } from "../../utils/formatPrice";
 import styles from "./Cart.module.css";
 import { getCartData } from "../../services/api/cart";
 import type { CartData } from "../../types/cart/CartData";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AppError } from "../../errors/AppError";
 import type { UpdateAction } from "../../types/cartItem/updateAction";
-import { deleteItem, updateQuantityItem } from "../../services/api/cartItem";
+import { deleteAllItems, deleteItem, updateQuantityItem } from "../../services/api/cartItem";
 
 // 	items: [
 // 		{
@@ -44,6 +44,7 @@ export default function Cart() {
 		items: [],
 		total: "0",
 	});
+	const [loading, setLoading] = useState<boolean>(false);
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -73,6 +74,8 @@ export default function Cart() {
 
 	const handleQuantityItem = async (action: UpdateAction, productId: number) => {
 		try {
+			setLoading(true);
+
 			const data = await updateQuantityItem(action, productId);
 
 			const updatedCartItems = cartData.items.map((item) =>
@@ -93,11 +96,15 @@ export default function Cart() {
 			}
 
 			alert("Erro inesperado");
+		} finally {
+			setLoading(false);
 		}
 	};
 
 	const handleDeleteItem = async (productId: number) => {
 		try {
+			setLoading(true);
+
 			const data = await deleteItem(productId);
 
 			const updatedCartItems = cartData.items.filter((item) => item.id !== productId);
@@ -117,6 +124,34 @@ export default function Cart() {
 			}
 
 			alert("Erro inesperado");
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const handleDeleteAllItems = async () => {
+		try {
+			setLoading(true);
+
+			const data = await deleteAllItems();
+
+			setCartData({ items: [], total: "0" });
+			alert(data.message);
+		} catch (error) {
+			if (error instanceof AppError) {
+				if (error.statusCode === 401) {
+					localStorage.removeItem("user");
+					navigate("/entrar");
+					return;
+				}
+
+				alert(error.message);
+				return;
+			}
+
+			alert("Erro inesperado");
+		} finally {
+			setLoading(false);
 		}
 	};
 
@@ -126,35 +161,56 @@ export default function Cart() {
 				<h1>Carrinho</h1>
 			</header>
 
-			<div className={styles.totalContainer}>
-				<span>Total</span>
-				<strong>{formatPrice(cartData.total)}</strong>
-			</div>
+			{cartData.items.length > 0 ? (
+				<>
+					<div className={styles.totalContainer}>
+						<span>Total</span>
+						<strong>{formatPrice(cartData.total)}</strong>
+					</div>
 
-			<div className={styles.clearButtonContainer}>
-				<button>Remover todos os produtos</button>
-			</div>
+					<div className={styles.clearButtonContainer}>
+						<button
+							onClick={() => handleDeleteAllItems()}
+							disabled={loading}
+						>
+							Remover todos os produtos
+						</button>
+					</div>
 
-			<ul className={styles.itemsList}>
-				{cartData.items.map((item) => (
-					<li key={item.id}>
-						<CartItem
-							id={item.id}
-							name={item.name}
-							price={item.price}
-							stock={item.stock}
-							imageUrl={item.imageUrl}
-							quantity={item.quantity}
-							handleQuantityItem={handleQuantityItem}
-							handleDeleteItem={handleDeleteItem}
-						/>
-					</li>
-				))}
-			</ul>
+					<ul className={styles.itemsList}>
+						{cartData.items.map((item) => (
+							<li key={item.id}>
+								<CartItem
+									id={item.id}
+									name={item.name}
+									price={item.price}
+									stock={item.stock}
+									imageUrl={item.imageUrl}
+									quantity={item.quantity}
+									handleQuantityItem={handleQuantityItem}
+									handleDeleteItem={handleDeleteItem}
+									loading={loading}
+								/>
+							</li>
+						))}
+					</ul>
 
-			<div className={styles.checkoutButtonContainer}>
-				<button>Finalizar pedido ({cartData.items.length} itens)</button>
-			</div>
+					<div className={styles.checkoutButtonContainer}>
+						<button>
+							Finalizar pedido (
+							{cartData.items.reduce((total, item) => {
+								return (total += item.quantity);
+							}, 0)}{" "}
+							itens)
+						</button>
+					</div>
+				</>
+			) : (
+				<div className={styles.emptyCartDescription}>
+					<p>Não há itens no carrinho</p>
+					<Link to="/">Voltar para a vitrine</Link>
+				</div>
+			)}
 		</section>
 	);
 }
