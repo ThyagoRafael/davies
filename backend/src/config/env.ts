e3c93afbf3180427ba43e085afc1d1e0c2618317
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-03 11:18:58 -0300

    refactor: adicionar coerce na validação da variável de ambiente da porta

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
-	PORT: z.string().default("3000"),
+	PORT: z.coerce.number().default(3000),
 	DATABASE_URL: z.string(),
 	JWT_SECRET: z.string(),
 	CLOUDINARY_NAME: z.string(),
