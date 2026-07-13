29dbbc3143d510efa81cc51bcaae75ec61ba9efa
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-13 18:41:57 -0300

    feat: cria o componente de select para forms

---
+++ frontend/src/components/form/SelectField/index.tsx
@@ -0,0 +1,38 @@
+interface SelectFieldProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
+	label: string;
+	name: string;
+	handleChange: (name: string, value: string) => void;
+	options: { value: string; label: string }[];
+	placeholder?: string;
+}
+
+export default function SelectField({ label, handleChange, options, placeholder, ...props }: SelectFieldProps) {
+	return (
+		<div>
+			<label htmlFor={props.name}>{label}</label>
+
+			<select
+				{...props}
+				id={props.name}
+				name={props.name}
+				onChange={(e) => handleChange(props.name, e.target.value)}
+			>
+				<option
+					value=""
+					disabled
+				>
+					{placeholder ?? "Selecione uma opção"}
+				</option>
+
+				{options.map((option) => (
+					<option
+						key={option.value}
+						value={option.value}
+					>
+						{option.label}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+}
