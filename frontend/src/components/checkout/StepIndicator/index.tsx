945bf4a475dc8592b162543ecb1d2ba785c96bfe
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-16 11:04:21 -0300

    styles: adiciona estilos ao indicador de etapas

--- frontend/src/components/checkout/StepIndicator/index.tsx
+++ frontend/src/components/checkout/StepIndicator/index.tsx
@@ -1,21 +1,44 @@
+import styles from "./StepIndicator.module.css";
+
 interface StepIndicatorProps {
 	actualStep: 1 | 2 | 3;
 }
 
 export default function StepIndicator({ actualStep }: StepIndicatorProps) {
 	return (
-		<nav aria-label="Etapas do checkout">
+		<nav
+			aria-label="Etapas do checkout"
+			className={styles.stepContainer}
+		>
 			<ol>
-				<li aria-current={actualStep === 1 ? "step" : undefined}>
-					<div aria-hidden="true"></div>
+				<li
+					aria-current={actualStep === 1 ? "step" : undefined}
+					className={styles.stepItem}
+				>
+					<div
+						aria-hidden="true"
+						className={actualStep === 1 ? styles.active : actualStep > 1 ? styles.success : ""}
+					></div>
 					<span>Endereço</span>
 				</li>
-				<li aria-current={actualStep === 2 ? "step" : undefined}>
-					<div aria-hidden="true"></div>
+				<li
+					aria-current={actualStep === 2 ? "step" : undefined}
+					className={styles.stepItem}
+				>
+					<div
+						aria-hidden="true"
+						className={actualStep === 2 ? styles.active : actualStep > 2 ? styles.success : ""}
+					></div>
 					<span>Pagamento</span>
 				</li>
-				<li aria-current={actualStep === 3 ? "step" : undefined}>
-					<div aria-hidden="true"></div>
+				<li
+					aria-current={actualStep === 3 ? "step" : undefined}
+					className={styles.stepItem}
+				>
+					<div
+						aria-hidden="true"
+						className={actualStep === 3 ? styles.active : ""}
+					></div>
 					<span>Confirmação</span>
 				</li>
 			</ol>
