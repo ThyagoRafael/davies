0ddced33fcdedcd17a764827fb5ed0da28d1ef7b
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-20 14:54:39 -0300

    style: adiciona estilos ao card de preço da confirmação do pedido

--- frontend/src/components/checkout/ConfirmationStep/PriceCard/index.tsx
+++ frontend/src/components/checkout/ConfirmationStep/PriceCard/index.tsx
@@ -1,17 +1,20 @@
 import { formatPrice } from "../../../../utils/formatPrice";
+import styles from "./PriceCard.module.css";
 
 export default function PriceCard() {
 	return (
-		<dl>
-			<div>
-				<dt>Itens</dt>
-				<dd>{formatPrice("150.00")}</dd>
+		<dl className={styles.container}>
+			<div className={styles.dataContainer}>
+				<div className={styles.dataGroup}>
+					<dt>Itens</dt>
+					<dd>{formatPrice("150.00")}</dd>
+				</div>
+				<div className={styles.dataGroup}>
+					<dt>Frete</dt>
+					<dd>{formatPrice("20.00")}</dd>
+				</div>
 			</div>
-			<div>
-				<dt>Frete</dt>
-				<dd>{formatPrice("20.00")}</dd>
-			</div>
-			<div>
+			<div className={styles.totalGroup}>
 				<dt>
 					<strong>Total</strong>
 				</dt>
