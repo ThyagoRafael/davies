8ef4bfe5dcc105a2e7526e915ae238f66d2e547c
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-20 09:37:29 -0300

    refactor: remove Fragment desnecessário

--- frontend/src/components/checkout/PaymentStep/CardPayment/CardItem/index.tsx
+++ frontend/src/components/checkout/PaymentStep/CardPayment/CardItem/index.tsx
@@ -8,25 +8,23 @@ interface CardItemProps {
 
 export default function CardItem({ card, checked, onSelect }: CardItemProps) {
 	return (
-		<>
-			<label>
-				<input
-					type="radio"
-					name="selected-card"
-					checked={checked}
-					onChange={() => onSelect(card)}
-				/>
-				<div>
-					<strong>
-						{card.cardBrand} ****{card.lastDigits}
-					</strong>
+		<label>
+			<input
+				type="radio"
+				name="selected-card"
+				checked={checked}
+				onChange={() => onSelect(card)}
+			/>
+			<div>
+				<strong>
+					{card.cardBrand} ****{card.lastDigits}
+				</strong>
 
-					<div>
-						<p>{card.holderName}</p>
-						<p>Expira em {card.expiryDate}</p>
-					</div>
+				<div>
+					<p>{card.holderName}</p>
+					<p>Expira em {card.expiryDate}</p>
 				</div>
-			</label>
-		</>
+			</div>
+		</label>
 	);
 }
