0a838d04db9cc9e1ea9c5dc8cfedfba725e54333
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-21 09:33:22 -0300

    style: adiciona estilos ao card de endereço na confirmação do pedido

--- frontend/src/components/checkout/ConfirmationStep/AddressCard/index.tsx
+++ frontend/src/components/checkout/ConfirmationStep/AddressCard/index.tsx
@@ -1,6 +1,7 @@
 import type { Address } from "../../../../types/api/address";
 import { formatPhoneNumber } from "../../../../utils/formatPhoneNumber";
 import { formatZipCode } from "../../../../utils/formatZipCode";
+import styles from "./AddressCard.module.css";
 
 interface AddressCardProps {
 	selectedAddress: Address | null;
@@ -8,22 +9,22 @@ interface AddressCardProps {
 
 export default function AddressCard({ selectedAddress }: AddressCardProps) {
 	return (
-		<section>
+		<section className={styles.container}>
 			<header>
 				<h2>Endereço de entrega</h2>
 			</header>
 
 			{selectedAddress && (
-				<article>
+				<article className={styles.detailsContainer}>
 					<h3>
-						<span>
+						<span className={styles.receiverName}>
 							<strong>{selectedAddress.receiverName}</strong>
 						</span>
-						<span>-</span>
-						<span>{formatPhoneNumber(selectedAddress.receiverPhone)}</span>
+						<span className={styles.separator}>-</span>
+						<span className={styles.phone}>{formatPhoneNumber(selectedAddress.receiverPhone)}</span>
 					</h3>
 
-					<address>
+					<address className={styles.addressDescription}>
 						{selectedAddress.street}
 						{selectedAddress.addressComplement && ` ${selectedAddress.addressComplement}`},{" "}
 						{selectedAddress.number}, {selectedAddress.city} - {selectedAddress.state},{" "}
