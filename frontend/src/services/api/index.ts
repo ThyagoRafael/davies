7bd5c5463cb9579f3edeaafec5729d3c3f207328
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-27 15:30:45 -0300

    feat: adiciona interceptor para enviar token nas requisições

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
 import { logoutAndRedirectToLogin } from "../../helpers/logout";
+import { getUserStorage } from "../../helpers/getUserStorage";
 
 const baseURL = import.meta.env.VITE_API_URL;
 const api = axios.create({ baseURL });
+
+api.interceptors.request.use((config) => {
+	const user = getUserStorage();
+
+	if (user) {
+		const { token } = user;
+
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+
+	return config;
+});
 
 api.interceptors.response.use(
 	(response) => response,
