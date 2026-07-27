9cd1c99f962648a56c583cd906e8e34af8711382
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-27 14:36:49 -0300

    feat: adiciona interceptor para tratar respostas 401

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
+import { logoutAndRedirectToLogin } from "../../helpers/logout";
 
 const baseURL = import.meta.env.VITE_API_URL;
 const api = axios.create({ baseURL });
+
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.response?.status === 401) {
+			logoutAndRedirectToLogin();
+		}
+
+		return Promise.reject(error);
+	},
+);
 
 export { api };
