673e34e2ed96d153037f903b2b38b92db747fa94
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-28 10:39:40 -0300

    feat: adiciona tratamento de erros do stripe

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,11 +1,17 @@
 import { type Request, type Response, type NextFunction } from "express";
 import { AppError } from "../errors/AppError.js";
+import Stripe from "stripe";
 
 export function errorMiddleware(err: unknown, req: Request, res: Response, _next: NextFunction) {
 	console.log(err);
 
 	if (err instanceof AppError) {
 		res.status(err.statusCode).json({ message: err.message });
+		return;
+	}
+
+	if (err instanceof Stripe.errors.StripeError) {
+		res.status(err.statusCode || 400).json({ message: err.message });
 		return;
 	}
 
