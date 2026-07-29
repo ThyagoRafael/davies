c040505d780f5783e1f5c5d4469664874047485c
ThyagoRafael <thyagorafael57@gmail.com>
2026-07-29 14:46:24 -0300

    feat: adiciona service createUserCard

--- frontend/src/services/api/userCard.ts
+++ frontend/src/services/api/userCard.ts
@@ -1,3 +1,12 @@
+import { api } from ".";
+import type { UserCard } from "../../types/api/userCard";
+
 export async function getUserCards() {
 	return [];
+}
+
+export async function createUserCard(data: { holderName: string; paymentMethodId: string }): Promise<UserCard> {
+	const response = await api.post("/paymentCards", data);
+
+	return response.data;
 }
