d7c10d21c84ad9b11a4ede61dec06bac4ad27c28
ThyagoRafael <thyagorafael57@gmail.com>
2026-08-03 19:26:04 -0300

    feat: adiciona tipagem no service de finalizar pedido

--- frontend/src/services/api/order.ts
+++ frontend/src/services/api/order.ts
@@ -1,6 +1,7 @@
 import { api } from ".";
+import type { Order } from "../../types/api/order";
 
-export async function finishOrder(addressId: number) {
+export async function finishOrder(addressId: number): Promise<Order> {
 	const response = await api.post("/order/checkout", {
 		addressId,
 	});
