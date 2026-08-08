3e6339001c090a78395415d8d39813c20b24ec8f
ThyagoRafael <thyagorafael57@gmail.com>
2026-08-08 16:29:34 -0300

    fix: altera rota /paymentCards para /payment-cards

--- frontend/src/services/api/userCard.ts
+++ frontend/src/services/api/userCard.ts
@@ -2,13 +2,13 @@ import { api } from ".";
 import type { UserCard } from "../../types/api/userCard";
 
 export async function getUserCards() {
-	const response = await api.get("/paymentCards");
+	const response = await api.get("/payment-cards");
 
 	return response.data;
 }
 
 export async function createUserCard(data: { holderName: string; paymentMethodId: string }): Promise<UserCard> {
-	const response = await api.post("/paymentCards", data);
+	const response = await api.post("/payment-cards", data);
 
 	return response.data;
 }
