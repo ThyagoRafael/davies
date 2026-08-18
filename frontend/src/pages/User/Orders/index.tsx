bf610ad6e83e3ebbe796834d4b7dc3021915efcf
ThyagoRafael <thyagorafael57@gmail.com>
2026-08-17 21:41:28 -0300

    feat: adiciona estrutura inicial da página de pedidos do usuário

--- frontend/src/pages/User/Orders/index.tsx
+++ frontend/src/pages/User/Orders/index.tsx
@@ -1,7 +1,31 @@
+import { formatPrice } from "../../../utils/formatPrice";
+
 export default function Orders() {
 	return (
 		<section>
-			<h1>Seus pedidos</h1>
+			<header>
+				<h1>Seus pedidos</h1>
+			</header>
+
+			<ul>
+				{[1, 2, 3].map((item) => (
+					<li key={item}>
+						<div>
+							<p>Pedido #PED-00000</p>
+							<p>Entregue</p>
+						</div>
+
+						<div>
+							<p>Entregue em 18 de agosto</p>
+
+							<div>
+								<p>3 itens</p>
+								<p>{formatPrice("554.98")}</p>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
 		</section>
 	);
 }
