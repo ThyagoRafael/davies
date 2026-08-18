3c92b649ff6c0750be03ee0eef99deee3b5642da
ThyagoRafael <thyagorafael57@gmail.com>
2026-08-18 10:01:21 -0300

    style: adiciona estilos à página de pedidos do usuário

--- frontend/src/pages/User/Orders/index.tsx
+++ frontend/src/pages/User/Orders/index.tsx
@@ -1,23 +1,33 @@
 import { formatPrice } from "../../../utils/formatPrice";
+import styles from "./Orders.module.css";
 
 export default function Orders() {
 	return (
-		<section>
-			<header>
+		<section className={styles.container}>
+			<header className={styles.header}>
 				<h1>Seus pedidos</h1>
 			</header>
 
-			<ul>
+			<ul className={styles.ordersContainer}>
 				{[1, 2, 3].map((item) => (
 					<li key={item}>
-						<article>
-							<div>
+						<article className={styles.orderCard}>
+							<div className={styles.orderHeader}>
 								<h2>Pedido #PED-00000</h2>
 								<p role="status">Entregue</p>
 							</div>
 
-							<div>
-								<p>3 itens</p>
+							<div className={styles.orderContent}>
+								<div className={styles.itemsContent}>
+									<p className={styles.quantity}>3 itens</p>
+									<p
+										className={styles.preview}
+										title="Camisa azul, Calça rosa, Tênis branco, Boné preto, Camisa branca"
+									>
+										Camisa azul, Calça rosa, Tênis branco, Boné preto, Camisa branca
+									</p>
+								</div>
+
 								<strong>{formatPrice("554.98")}</strong>
 							</div>
 						</article>
