1f5f6fe4b7ff25ad1283518bbda30d5044450d2e
ThyagoRafael <thyagorafael57@gmail.com>
2026-08-20 10:43:21 -0300

    style: adiciona estilos à página de detalhes do pedido

--- frontend/src/pages/User/Orders/Details/index.tsx
+++ frontend/src/pages/User/Orders/Details/index.tsx
@@ -1,11 +1,11 @@
 import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import imageTeste from "../../../../assets/imagem-teste.png";
-import { formatPrice } from "../../../../utils/formatPrice";
 import styles from "./Details.module.css";
 import PriceCard from "../../../../components/order/PriceCard";
 import type { PaymentStatus } from "../../../../types/api/payment";
 import AddressCard from "../../../../components/order/AddressCard";
+import OrderItemsContainer from "../../../../components/order/OrderItemsContainer";
 
 export default function OrderDetails() {
 	// const { orderId } = useParams();
@@ -32,21 +32,23 @@ export default function OrderDetails() {
 
 			<section className={styles.detailsContainer}>
 				<header className={styles.detailsHeader}>
-					<h2>Pedido entregue em 19/08/2026</h2>
+					<div className={styles.orderInfo}>
+						<h2>Pedido entregue em 19/08/2026</h2>
 
-					<div>
-						<p>Pedido realizado em 15/08/2026</p>
-						<p>Código do pedido #PED-00000</p>
+						<div>
+							<p>Pedido realizado em 15/08/2026</p>
+							<p>Código do pedido #PED-00000</p>
+						</div>
 					</div>
-				</header>
 
-				<PriceCard
-					priceData={{
-						itemsPrice: "150.00",
-						shippingPrice: "5.00",
-						totalPrice: "155.00",
-					}}
-				/>
+					<PriceCard
+						priceData={{
+							itemsPrice: "150.00",
+							shippingPrice: "5.00",
+							totalPrice: "155.00",
+						}}
+					/>
+				</header>
 
 				<section className={styles.detailsSection}>
 					<h2>Pagamento</h2>
@@ -77,32 +79,9 @@ export default function OrderDetails() {
 					}}
 				/>
 
-				<section className={styles.detailsSection}>
-					<h2>Itens do pedido</h2>
-
-					<ul className={styles.itemsList}>
-						{[1, 2, 3].map((item) => (
-							<li key={item}>
-								<div className={styles.itemContent}>
-									<div className={styles.imageWrapper}>
-										<img
-											src={imageTeste}
-											alt=""
-										/>
-									</div>
-									<div>
-										<h3>Camisa azul</h3>
-
-										<footer>
-											<p>Quantidade: 3</p>
-											<strong>{formatPrice("150.00")}</strong>
-										</footer>
-									</div>
-								</div>
-							</li>
-						))}
-					</ul>
-				</section>
+				<OrderItemsContainer
+					items={[{ id: 1, name: "Produto teste", imageUrl: imageTeste, price: "105.00", quantity: 8 }]}
+				/>
 			</section>
 		</section>
 	);
