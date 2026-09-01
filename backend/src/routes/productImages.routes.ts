d3ca10a47cdb9a93f28cfe5d11f82a5c967ba46b
ThyagoRafael <thyagorafael57@gmail.com>
2026-09-01 10:40:39 -0300

    feat: protege rotas de imagens dos produtos com auth

--- backend/src/routes/productImages.routes.ts
+++ backend/src/routes/productImages.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { ProductImagesController } from "../controllers/productImages.controller.js";
 import { uploadMiddleware } from "../middlewares/upload.middleware.js";
+import { authenticationMiddleware, authorizationMiddleware } from "../middlewares/auth.middleware.js";
 
 const productImageRoutes = Router({ mergeParams: true });
 const productImagesController = new ProductImagesController();
 
-productImageRoutes.post("/images", uploadMiddleware.array("images", 8), productImagesController.upload);
-productImageRoutes.put("/images", productImagesController.update);
+productImageRoutes.post(
+	"/images",
+	authenticationMiddleware,
+	authorizationMiddleware,
+	uploadMiddleware.array("images", 8),
+	productImagesController.upload,
+);
+productImageRoutes.put("/images", authenticationMiddleware, authorizationMiddleware, productImagesController.update);
 
 export { productImageRoutes };
